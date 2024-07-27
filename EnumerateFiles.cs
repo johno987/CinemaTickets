ec325b1a4e56eaf020d637b2b504bb79c3b76f6a
johno987 <willjohnson987@outlook.com>
2024-07-27 22:54:59 +0100

    Added error handing to the PDF read

--- EnumerateFiles.cs
+++ EnumerateFiles.cs
@@ -1,11 +1,30 @@
 ﻿
 //enumerate the files within the directory to find those ending with .pdf and add them to a list to be read
+using System.Net.WebSockets;
+
 internal static class EnumerateFiles
 {
     public static List<string> pdfFiles = new(); //contains the file path to the 3 sets of tickets to be read
     public static void readFiles()
     {
-        var localDirectory = AppDomain.CurrentDomain.BaseDirectory; //gets directory where the exe is placed and subsequently where the pdf files are stored in this instance
-        pdfFiles = Directory.GetFiles($"{localDirectory}", "*.pdf", searchOption: SearchOption.AllDirectories).ToList(); //gets the full file path to all the pdf files within the directory we specify above
+        try
+        {
+            var localDirectory = AppDomain.CurrentDomain.BaseDirectory; //gets directory where the exe is placed and subsequently where the pdf files are stored in this instance
+            pdfFiles = Directory.GetFiles($"{localDirectory}", "*.pdf", searchOption: SearchOption.AllDirectories).ToList(); //gets the full file path to all the pdf files within the directory we specify above
+        }
+        catch (AppDomainUnloadedException ex)
+        {
+            Console.WriteLine("Files could not be read from the list directory", ex.Message);
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine("Invalid argument passed to GetFiles", ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("The program could not read from the specified file path, ending the program");
+            Environment.Exit(-1);
+        }
+
     }
 }
--- obj/Debug/net8.0/CinemaTickets.AssemblyInfo.cs
+++ obj/Debug/net8.0/CinemaTickets.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaTickets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9493f088c5fce8ea0105c6d68803811afaa3540b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33a61eb8326973ef9cb3cccfa06a8ea8e878a265")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaTickets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaTickets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
